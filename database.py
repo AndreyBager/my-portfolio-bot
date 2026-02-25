9a8f992c41f4f9b3caf6bd7a238417e49aad7b75
AndreyBager <bager.interface@gmail.com>
2026-02-25 12:52:51 +1000

    Refactor database.py for SQLite connection and functions

--- database.py
+++ database.py
@@ -1,9 +1,8 @@
-from sqlalchemy import String, Integer, ForeignKey, select, delete
+from sqlalchemy import String, Integer, select, delete
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 from sqlalchemy.ext.asyncio import AsyncAttrs, async_sessionmaker, create_async_engine
 
-# 1. Настройка подключения (Переходим на SQLite для простоты)
-# Этот адрес создаст файл test.db прямо на сервере
+# 1. Настройка подключения к SQLite
 engine = create_async_engine(url='sqlite+aiosqlite:///./test.db')
 
 async_session = async_sessionmaker(engine)
@@ -18,12 +17,13 @@ class Item(Base):
     id: Mapped[int] = mapped_column(primary_key=True)
     name: Mapped[str] = mapped_column(String(100))
     description: Mapped[str] = mapped_column(String(500))
+    # nullable=True позволяет добавлять работы БЕЗ фото
     photo_id: Mapped[str] = mapped_column(String(255), nullable=True) 
     category: Mapped[str] = mapped_column(String(50))
 
 # --- ФУНКЦИИ ДЛЯ РАБОТЫ С БАЗОЙ ---
 
-# Создание таблиц (запускается при старте)
+# Создание таблиц
 async def async_main():
     async with engine.begin() as conn:
         await conn.run_sync(Base.metadata.create_all)
@@ -34,9 +34,8 @@ async def get_items_by_category(category_name):
         result = await session.execute(select(Item).where(Item.category == category_name))
         return result.scalars().all()
 
-# Удаление работы
+# Удаление работы из базы
 async def delete_item_from_db(item_id: int):
     async with async_session() as session:
         async with session.begin():
             await session.execute(delete(Item).where(Item.id == item_id))
-
